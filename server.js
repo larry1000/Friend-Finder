a8eb1eadbcfce7009f93bb844fcfa43742a8f5fc
Larry L <larrylight2003@msn.com>
2018-11-18 23:03:49 -0500

    more tweaks

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 let express = require("express");
 let app = express();
-var bodyParser = require("body-parser");
+// var bodyParser = require("body-parser");
 
 const PORT = process.env.PORT || 3000;
 
 // MIDDLEWARE
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 // parse application/json
-app.use(bodyParser.json());
+app.use(express.json());
 
 require("./app/routing/htmlRoutes")(app);
 require("./app/routing/apiRoutes")(app);
